554291383499bef7dc81042af0b430ef73a16518
Deijux <juanchocruz200516@gmail.com>
2022-06-18 12:47:45 -0500

    Programa version 1.0

---
+++ calculadora.py
@@ -0,0 +1,31 @@
+import time
+
+def intro():
+    print("-------------------- Calcular ganancias ventas ---------------------")
+    print("---------------------------- Bienvenido ----------------------------")
+    print("-------- Asegure de usar el punto para especificar los miles -------")
+
+intro()
+
+def operacion():
+    pre_ven = float(input("¿Cual es el precio del producto en venta? : "))
+    pre_pro = float(input("¿Cual es el precio del producto por el proveedor? : "))
+    ven_mes = int(input("¿Cuantos producos del mismo se vendió? : "))
+    
+    rest = (pre_ven)-(pre_pro)
+    marg_gan = (rest)*(ven_mes)
+    print("Su margen de ganancia de los " +str(ven_mes) + " productos es: {:.3f}".format(marg_gan))
+    
+    time.sleep(5)
+    
+    print("--------------------------------------------------------------------------")
+    print("--------------------------------------------------------------------------")
+    
+    decs = input("¿Quieres hacer otro calculo? (si, no): ")
+    if decs == "si":
+        return operacion()
+    if decs == "no":
+        print("Gracias por utilizarme, ten un buen día :D")
+        time.sleep(5)
+
+operacion()
